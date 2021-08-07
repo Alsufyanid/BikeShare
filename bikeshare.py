36a58b2ef09666fb07e490f3d2cb6e1bfdfd3991
Daniah-Abdulaziz-Alsufyani <dani_suf@windowslive.com>
2021-08-07 18:25:13 +0300

    remove display data function

--- bikeshare.py
+++ bikeshare.py
@@ -156,6 +156,8 @@ def user_stats(df,city):
 
     
 
+
+
 def main():
     while True:
         city, month, day = get_filters()
@@ -165,7 +167,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df,city)
-       
+      
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
